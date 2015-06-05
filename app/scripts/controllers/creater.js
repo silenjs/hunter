e8bfcfe74d131beacb4722e3f677c6f10d25eea1
silen <silenjs@gmail.com>
2015-06-06 00:26:17 +0800

    update

--- app/scripts/controllers/creater.js
+++ app/scripts/controllers/creater.js
@@ -1,25 +1,38 @@
 var modCreater = angular.module('moduleCreater',[]);
 modCreater.controller('CreaterController',function($scope){
-    var bodyW=0,bodyH=0,curPage=1,totalPage=4;
-    var initPage = function(){
-        bodyW = window.innerWidth;
-        bodyH = window.innerHeight;
-        $('#wraper').css({'width':bodyW,'height':bodyH});
-        $('#wraper form').each(function(index,item){
-            $(item).css({'width':bodyW,'height':bodyH});
-        })
-    }
-    var nextPage = function(){
-        if(curPage<totalPage){
-            $('#wraper').css('transform','translateY('+-curPage*bodyH+'px)');
-            curPage++;
-        }
-        $('#nextTrigger')[curPage>=totalPage?'hide':'show']();
-    }
-    window.onresize =  initPage;
-
-    $scope.nextTrigger = function(){
-        nextPage();
-    }
-    initPage();
+    
 })
+modCreater.directive('creater',function(){
+    return {
+        restrict:'AE',
+        replace:true,
+        templateUrl:'views/createrDetail.html',
+        link:function(scope,elem,attr){
+            var bodyW=0,bodyH=0,curPage=1,totalPage=4;
+            var initPage = function(){
+                bodyW = window.innerWidth,bodyH = window.innerHeight;
+                $('#wraper').css({'width':bodyW,'height':bodyH});
+                $('#wraper form').each(function(index,item){
+                    $(item).css({'width':bodyW,'height':bodyH});
+                })
+            }
+            var nextPage = function(){
+                if(curPage<totalPage){
+                    $('#wraper').css('transform','translateY('+-curPage*bodyH+'px)');
+                    curPage++;
+                }
+                $('#nextTrigger')[curPage>=totalPage?'hide':'show']();
+            }
+            $('#nextTrigger').on('click',function(){
+                nextPage();
+            })
+            window.onresize =  initPage;
+            initPage();
+        }
+    }    
+})
+
+
+/*
+
+*/
